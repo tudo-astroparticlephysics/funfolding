e0ae754d791b35ba34cc0192c7d4d5c10bd1363f
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-01-26 11:56:57 +0100

    modified sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,9 +32,10 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+              'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -82,7 +83,14 @@ todo_include_todos = False
 
 
 # -- Options for HTML output ----------------------------------------------
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    print('No sphinx_rtd_theme found! Default theme is used!')
+else:
+    html_theme = "sphinx_rtd_theme"
 
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
