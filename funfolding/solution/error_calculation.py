6fca0c5e418695c8f3d9ef886779e0cfb11d9a75
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-06-14 12:19:40 -0600

    debugging feldman, cousins

--- funfolding/solution/error_calculation.py
+++ funfolding/solution/error_calculation.py
@@ -4,13 +4,13 @@ import numpy as np
 def calc_feldman_cousins_errors(best_fit, sample, interval=0.68):
     diff = np.absolute(sample - best_fit)
     sample_length = sample.shape[0]
-    order = np.argsort(diff)
     n_events = int(sample_length * interval) + 1
-    select = order[:n_events]
-    selected_sample = sample[select, :]
     sigma_vec_best = np.zeros((2, len(best_fit)))
-
-    sigma_vec_best[0, :] = np.min(selected_sample, axis=0)
-    sigma_vec_best[1, :] = np.max(selected_sample, axis=0)
+    for i in range(len(best_fit)):
+        order = np.argsort(diff[:, i])
+        select = order[:n_events]
+        selected_sample = sample[select, i]
+        sigma_vec_best[0, i] = np.min(selected_sample, axis=0)
+        sigma_vec_best[1, i] = np.max(selected_sample, axis=0)
 
     return sigma_vec_best
