e716e3b7089cddae4b6b6a1c126fa39deff44ac9
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-06-14 19:52:27 +0200

    debugging feldman

--- funfolding/solution/error_calculation.py
+++ funfolding/solution/error_calculation.py
@@ -8,7 +8,7 @@ def calc_feldman_cousins_errors(best_fit, sample, interval=0.68):
     n_events = int(sample_length * interval) + 1
     select = order[:n_events]
     selected_sample = sample[select, :]
-    sigma_vec_best = np.zeroes((2, len(best_fit)))
+    sigma_vec_best = np.zeros((2, len(best_fit)))
 
     sigma_vec_best[0, :] = np.min(selected_sample, axis=0)
     sigma_vec_best[1, :] = np.max(selected_sample, axis=0)
