25115c264477d7f27f805a006d7e81b36b434d48
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-09-11 15:54:29 +0200

    fixed import bug

--- funfolding/pipeline/__init__.py
+++ funfolding/pipeline/__init__.py
@@ -1,8 +1,12 @@
+'''In this module different elements of a typical unfolding analysis are
+implemented.
+'''
 from ._pipeline import split_test_unfolding
-from ._feature_selection import rec_feature_selection_condition_validation
-from ._feature_selection import rec_feature_selection_condition_cv
+from ._feature_selection import \
+    recursive_feature_selection_condition_validation
+from ._feature_selection import recursive_feature_selection_condition_cv
 
 
 __all__ = ['split_test_unfolding',
-           'rec_feature_selection_condition_cv',
-           'rec_feature_selection_condition_validation']
+           'recursive_feature_selection_condition_cv',
+           'recursive_feature_selection_condition_validation']
