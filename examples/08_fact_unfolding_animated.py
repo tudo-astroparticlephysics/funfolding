1ca7736f840bf8aa46c8cf8c6a9980a324b2bc05
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-09-14 15:47:00 +0200

    added animated fact unfolding example

--- examples/08_fact_unfolding_animated.py
+++ examples/08_fact_unfolding_animated.py
@@ -2,9 +2,6 @@ import logging
 
 import pandas as pd
 import numpy as np
-import matplotlib
-
-matplotlib.use('Agg')
 
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
@@ -27,6 +24,10 @@ if __name__ == '__main__':
     n_samples_test = 5000
     min_samples_leaf = 20
     binning_E = np.linspace(2.4, 4.2, 10)
+
+    logging.info('Running FACT Unfolding Example Animated Gradient Descent')
+    logging.info('========================================================')
+    logging.info('Loading Data')
 
 
     random_state = np.random.RandomState(random_seed)
@@ -76,12 +77,11 @@ if __name__ == '__main__':
     obs_array_binning = obs_array[idx_binning]
     obs_array_A = obs_array[idx_A]
 
+    logging.info('Fitting Observable Binning')
+
     tree_binning = binning.TreeBinningSklearn(
         regression=False,
-        max_features=None,
-        min_samples_split=2,
-        max_depth=None,
-        min_samples_leaf=min_samples_leaf * 10.,
+        min_samples_leaf=int(min_samples_leaf * 10),
         random_state=random_state)
 
     tree_binning.fit(obs_array_binning,
@@ -101,14 +101,14 @@ if __name__ == '__main__':
                                C='thikonov')
     llh.initialize(vec_g=vec_g,
                     model=tree_model)
-
+    logging.info('Starting Gradient Descent')
     sol_gd = solution.LLHSolutionGradientDescent(n_steps=500,
                                                  gamma=0.01)
     sol_gd.initialize(llh=llh, model=tree_model)
     sol_gd.set_x0_and_bounds()
     points, llh_values, gradient, hessian = sol_gd.fit()
     idx_best = np.argmax(llh_values)
-
+    logging.info('Starting MCMC')
     sol_mcmc = solution.LLHSolutionMCMC(n_burn_steps=100,
                                         n_used_steps=1000,
                                         n_walker=100,
@@ -120,7 +120,8 @@ if __name__ == '__main__':
     corner_fig = corner.corner(sample,
                                rasterized=True)
 
-    def init():
+
+    def first_init():
         axes = corner_fig.axes
         pointer = -1
         line_dict = {}
@@ -131,65 +132,99 @@ if __name__ == '__main__':
                 if i_x > i_y:
                     continue
                 elif i_x == i_y:
-                    continue
+                    line_dict[(i_x, i_y)] = ax.plot([], [], '-',
+                                                    lw=2.,
+                                                    color='b')[0]
                 else:
-                    line_dict[(i_x, i_y)] = ax.plot([], [], 'o-', color='b')
+                    line_dict[(i_x, i_y)] = ax.plot([], [], 'o-', color='b')[0]
         return line_dict
 
-    line_dict = init()
+
+    line_dict = first_init()
+
+    def clear():
+        logging.info('\tClearing Plot')
+        lines = []
+        for k, line in line_dict.items():
+            line.set_data([], [])
+            lines.append(line)
+        return lines
+
 
     def run(data_dict):
         pointer = -1
         lines = []
+        if data_dict['finished']:
+            return clear()
         for i_y in range(points.shape[1]):
             for i_x in range(points.shape[1]):
                 pointer += 1
-                line_i_j = line_dict[(i_x, i_y)]
-                try:
-                    p_x, p_y = data_dict[(i_x, i_y)]
-                except KeyError:
+                if i_x > i_y:
                     continue
                 else:
-                    line_i_j.set_data(p_x, p_y)
-                    lines.append(line_i_j)
-
+                    line_i_j = line_dict[(i_x, i_y)]
+                    try:
+                        p_x, p_y = data_dict[(i_x, i_y)]
+                    except KeyError:
+                        continue
+                    else:
+                        line_i_j.set_data(p_x, p_y)
+                        lines.append(line_i_j)
         return lines
+
 
     def gen_data(step_size=10):
         used_points = points[::step_size]
         axes = corner_fig.axes
 
+        total_steps = len(used_points)
+
         for n in range(len(used_points)):
+            logging.info('\tStep: {}/{}'.format(n, total_steps))
             data_dict = {}
+            data_dict['finished'] = False
             pointer = -1
-            points = used_points[:n+1]
-            for i_y in range(points.shape[1]):
-                for i_x in range(points.shape[1]):
+            used_points_n = used_points[:n+1]
+            for i_y in range(used_points_n.shape[1]):
+                for i_x in range(used_points_n.shape[1]):
                     pointer += 1
-                    ax = axes[pointer]
                     if i_x > i_y:
                         continue
-                    elif i_x == i_y:
-                        continue
+                    ax = axes[pointer]
+                    x_lims = ax.get_xlim()
+                    y_lims = ax.get_ylim()
+                    if i_x == i_y:
+                        x_pos = used_points_n[-1, i_x]
+                        x_in_range = np.logical_and(x_lims[0] < x_pos,
+                                                    x_lims[1] > x_pos)
+                        if x_in_range:
+                            data_dict[(i_x, i_y)] = [(x_pos, x_pos), y_lims]
                     else:
-                        x_lims = ax.get_xlim()
-                        y_lims = ax.get_ylim()
-                        p_x = points[:, i_x]
-                        p_y = points[:, i_y]
+                        p_x = used_points_n[:, i_x]
+                        p_y = used_points_n[:, i_y]
                         x_in_range = np.logical_and(x_lims[0] < p_x,
                                                     x_lims[1] > p_x)
                         y_in_range = np.logical_and(y_lims[0] < p_y,
                                                     y_lims[1] > p_y)
                         in_range = np.logical_and(x_in_range, y_in_range)
                         if sum(in_range) > 0:
-                            p_x = points[in_range, i_x]
-                            p_y = points[in_range, i_y]
+                            p_x = used_points_n[in_range, i_x]
+                            p_y = used_points_n[in_range, i_y]
                             data_dict[(i_x, i_y)] = (p_x, p_y)
             yield data_dict
+        data_dict = {}
+        data_dict['finished'] = False
+        yield data_dict
+
+
+
+    logging.info('Showing animation')
 
     ani = animation.FuncAnimation(corner_fig,
                                   run,
                                   gen_data,
-                                  interval=10,
-                                  repeat=True)
+                                  interval=50,
+                                  repeat=True,
+                                  blit=True,
+                                  init_func=clear)
     plt.show()
