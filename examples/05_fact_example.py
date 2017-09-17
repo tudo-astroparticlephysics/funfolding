fixing gradient hesse reg

examples/05_fact_example.py
@@ -34,13 +34,11 @@ def generate_acceptance_correction(vec_f_truth,
     return vec_acceptance
 
 
-
 if __name__ == '__main__':
     logging.captureWarnings(True)
     logging.basicConfig(
         format='%(processName)-10s %(name)s %(levelname)-8s %(message)s',
         level=logging.INFO)
-
 
     random_seed = 1340
     n_walkers = 100
@@ -48,9 +46,7 @@ if __name__ == '__main__':
     n_samples_test = 5000
     min_samples_leaf = 20
     tau = 1.
-
     binning_E = np.linspace(2.4, 4.2, 10)
-
 
     random_state = np.random.RandomState(random_seed)
     if not os.path.isfile('fact_simulations.hdf'):
@@ -78,7 +74,6 @@ if __name__ == '__main__':
     binned_E_test = binned_E[idx_test]
     binned_E_binning = binned_E[idx_binning]
     binned_E_A = binned_E[idx_A]
-
 
     obs_array = df.get(['log10(ConcCore)', 'log10(E_RF)']).values
 
@@ -259,8 +254,7 @@ if __name__ == '__main__':
                                vec_acceptance=vec_acceptance,
                                C='thikonov')
     llh.initialize(vec_g=vec_g,
-                    model=tree_model)
-
+                   model=tree_model)
 
     sol_gd = solution.LLHSolutionGradientDescent(n_steps=500,
                                                  gamma=0.01)
@@ -273,7 +267,6 @@ if __name__ == '__main__':
     logging.info('Best Fit (Gradient):\t{}\t(LLH: {})'.format(
         vec_f_str,
         llh_values[idx_best]))
-
 
     sol_mini = solution.LLHSolutionMinimizer()
     sol_mini.initialize(llh=llh, model=tree_model)
@@ -300,14 +293,14 @@ if __name__ == '__main__':
         vec_f_str,
         max(probs)))
 
-    sol_mcmc.n_threads = 9
-    logging.info('Calculating Eff sample size:')
-    n_eff = sol_mcmc.calc_effective_sample_size(sample, n_threads=9)
-    n_eff_str = ', '.join(str(n) for n in n_eff)
-    logging.info('per Walker:\t{} ({} Walker with {} steps)'.format(
-        n_eff_str,
-        n_walkers,
-        n_steps_used))
+    # sol_mcmc.n_threads = 9
+    # logging.info('Calculating Eff sample size:')
+    # n_eff = sol_mcmc.calc_effective_sample_size(sample, n_threads=9)
+    # n_eff_str = ', '.join(str(n) for n in n_eff)
+    # logging.info('per Walker:\t{} ({} Walker with {} steps)'.format(
+    #     n_eff_str,
+    #     n_walkers,
+    #     n_steps_used))
 
     def create_llh_slice(llh, best_fit, selected_bin=None):
         if selected_bin is None:
@@ -336,7 +329,11 @@ if __name__ == '__main__':
             hess_lower_y = gradient_values[i] - (diff * hessian_values[i])
             hess_upper_y = gradient_values[i] + (diff * hessian_values[i])
 
-            ax.arrow(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k')
+            ax.arrow(0, 0, 0.5, 0.5,
+                     head_width=0.05,
+                     head_length=0.1,
+                     fc='k',
+                     ec='k')
 
             ax_grad.plot([lower_x, upper_x],
                          [grad_lower_y, grad_upper_y],

funfolding/solution/_solution.py
@@ -172,9 +172,10 @@ class LLHSolutionGradientDescent(LLHSolutionMinimizer):
         hessian[0, :, :] = self.llh.evaluate_hessian(self.x0)
 
         for i in range(1, self.n_steps):
-            H_inv = linalg.inv(hessian[i-1])
-            delta_x = -np.dot(H_inv, gradient[i-1, :]) * self.gamma
+            H_inv = linalg.inv(hessian[i - 1])
+            delta_x = -np.dot(H_inv, gradient[i - 1, :]) * self.gamma
             x[i, :] = x[i - 1, :] + delta_x
+            x[i, x[i, :] < 0] = 0.
             llh[i] = self.llh.evaluate_llh(x[i, :])
             gradient[i, :] = self.llh.evaluate_gradient(x[i, :])
             hessian[i, :, :] = self.llh.evaluate_hessian(x[i, :])

funfolding/solution/likelihood.py
@@ -151,14 +151,18 @@ class StandardLLH(LLH):
                 f_used = f_reg * self.vec_acceptance + 1
                 ln_f_used = np.log(f_used)
                 ln_10_squared = np.log(10)**2
+                pre = np.zeros((self.model.dim_f,
+                                self.model.dim_f))
                 for i in range(self.model.dim_f):
                     for j in range(self.model.dim_f):
-                        reg_part_ij = self.vec_acceptance[i] * self._C[i, j]
-                        reg_part_ij *= ln_f_used[j]
-                        reg_part_ij /= ln_10_squared * f_used[i]
-                        if i == j:
-                            reg_part_ij *= 2.
-                        reg_part[i] += reg_part_ij
+                        pre_part_ij = self.vec_acceptance[i] * self._C[i, j]
+                        pre_part_ij *= ln_f_used[j]
+                        pre_part_ij /= ln_10_squared * f_used[i]
+                        pre[i] += pre_part_ij
+                for i in range(self.model.dim_f):
+                    reg_part_i = np.sum(pre[i, :])
+                    reg_part_i += np.sum(pre[:, i])
+
             else:
                 reg_part = np.dot(self._C, f_reg * self.vec_acceptance)
         else:
@@ -187,26 +191,24 @@ class StandardLLH(LLH):
                     for j in range(self.model.dim_f):
                         p_i_j = self._C[i, j] * self.vec_acceptance[i]**2
                         if i == j:
-                            p_i_j *= (ln_f_used[i] - 1) * 2
+                            p_i_j *= ln_f_used[i] - 1
                         else:
                             p_i_j *= ln_f_used[j]
                         p_i_j /= ln_10_squared * f_used[i]**2
                         pre_diag[i, j] = p_i_j
-
-                        if i == j:
-                            p_i_j = 0.
-                        else:
+                        if i != j:
                             p_i_j = self._C[i, j] * self.vec_acceptance[i] * \
                                 self.vec_acceptance[j]
-                            p_i_j *= ln_f_used[j]
                             p_i_j /= ln_10_squared * f_used[i] * f_used[j]
-                        pre_nondiag[i, j] = p_i_j
+                            pre_nondiag[i, j] = p_i_j
                 for i in range(self.model.dim_f):
                     for j in range(i + 1):
                         if i == j:
                             reg_part[i, i] = np.sum(pre_diag[i, :])
+                            reg_part[i, i] += np.sum(pre_diag[:, i])
                         else:
-                            r = np.sum(pre_nondiag[i, :])
+                            r = pre_nondiag[j, j]
+                            r += pre_nondiag[i, j]
                             reg_part[i, j] = r
                             reg_part[j, i] = r
             else: