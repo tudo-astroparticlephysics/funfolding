d154c22cde2dd0d9116af323f425c23715de7b30
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-09-26 11:01:47 +0200

    change rec fs function name

--- funfolding/pipeline/__init__.py
+++ funfolding/pipeline/__init__.py
@@ -4,9 +4,9 @@ implemented.
 from ._pipeline import split_test_unfolding
 from ._feature_selection import \
     recursive_feature_selection_condition_validation
-from ._feature_selection import recursive_feature_selection_condition_cv
+from ._feature_selection import recursive_feature_selection_condition_pulls
 
 
 __all__ = ['split_test_unfolding',
-           'recursive_feature_selection_condition_cv',
+           'recursive_feature_selection_condition_pulls',
            'recursive_feature_selection_condition_validation']
